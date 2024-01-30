1a23785ab5c55f2b112f26d725f738951d37cdec
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2024-01-30 16:00:10 +0700

    add : Shadcn Components

--- src/app/room_detail/page.jsx
+++ src/app/room_detail/page.jsx
@@ -1,8 +1,26 @@
+import PrimaryBtn from "@/components/common/PrimaryBtn";
+import SecondaryBtn from "@/components/common/SecondaryBtn";
+import DatePicker from "@/components/ui/DatePicker";
+import { DatePickerWithRange } from "@/components/ui/DatePickerWithRange";
+
 export default function RoomDetail() {
   return (
     <main>
-      <h1>Room Detail</h1>
+      <h5>Room Detail Page</h5>
+      <div className="search-bar bg-white shadow">
+        <div className="select-date flex flex-col">
+          <div className=" flex flex-col">
+            <p className=" font-sans text-base font-normal text-[#2a2e3f]">
+              Checkin - Checkout
+            </p>
+            <DatePicker />
+            <DatePickerWithRange />
+          </div>
 
+          <SecondaryBtn btnName="Search" />
+        </div>
+        *เดี๊ยวค่อยเอาไปใส่component
+      </div>
       <div className="room-card">
         <img
           className=" shadow-lg"
@@ -11,34 +29,22 @@ export default function RoomDetail() {
         />
         <div className="room-detail">
           <div>
-            <h1>Superior Garden View</h1>
-            <span>2 Guests | 2 Double bed | 32 sqm</span>
-            <p>
+            <h4>Superior Garden View</h4>
+            <body1>2 Guests | 2 Double bed | 32 sqm</body1>
+            <body1>
               Rooms (36sqm) with full garden views, 1 single bed, bathroom with
               bathtub & shower. 555
-            </p>
+            </body1>
           </div>
           <div>
-            <p>THB 3,100.00 เปลี่ยนสีจาก baseได้</p>
+            <p>THB 3,100.00</p>
             <h2>THB 2,500.00</h2>
             <p>Per Night</p>
             <p>(Including Taxes & Fees)</p>
           </div>
           <div>
             <p className=" visitlink">Room Detail</p>
-            <button className=" btn-primary btn-primary:hover btn-primary:visited">
-              Book Now
-            </button>
-
-            <button className=" btn-secondary ">Book Now</button>
-          </div>
-          <h4>Status</h4>
-          <div>
-            <p className="status-green">Vacant</p>
-            <p className="status-blue">Occupied</p>
-            <p className="status-red">Assign</p>
-            <p className="status-yellow">Inspected</p>
-            <p className="status-gray">out of Order</p>
+            <PrimaryBtn btnName="Book Now" />
           </div>
         </div>
       </div>
