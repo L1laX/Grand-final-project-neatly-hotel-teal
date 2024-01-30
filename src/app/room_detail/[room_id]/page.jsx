9008e099164e96e47c6d05793cc13a039b476430
Tai <5506726tai@gmail.com>
2024-01-30 11:56:14 +0700

    refresh

--- src/app/room_detail/[room_id]/page.jsx
+++ src/app/room_detail/[room_id]/page.jsx
@@ -1,9 +1,11 @@
-
 //// edit it
+"use client";
 export default function RoomDetailById({ params }) {
   return (
     <main>
-      <h1>Room Detail By Id No.{params.room_id}</h1>
+      <div className=" bg-black xl:bg-red-500">
+        <h1>Room Detail By Id No.{params.room_id}</h1>
+      </div>
     </main>
   );
 }
