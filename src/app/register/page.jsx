a8ed803e92fccb0642aad3b9d4ed1dc93dbc5e36
Name <pobparis.bun@gmail.com>
2024-01-31 10:44:29 +0700

    feat:add register page

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,5 +1,182 @@
+"use client";
+
+import PrimaryBtn from "@/components/common/PrimaryBtn";
+
 const Register = () => {
-  return <div>Register Page</div>;
+  return (
+    <div
+      className="flex h-[1500px] w-auto items-center justify-center  bg-black p-20 md:h-[1777px] md:w-auto"
+      style={{ backgroundImage: "url('your-background-image.jpg')" }}
+    >
+      {/* Form Header */}
+      <from className="  h-[1300px] items-center justify-center  rounded bg-slate-50  p-10  shadow md:h-[1626px] md:w-[1092px]   ">
+        <div className=" md:flex-col">
+          <h2 className="mb-4 text-start text-[50px] font-bold  md:mb-[50px] md:text-start">
+            Register
+          </h2>
+          <h4 className=" text-start text-sm md:mb-[50px] md:text-start">
+            BasicInformation
+          </h4>
+        </div>
+        <div className="md:flex-col">
+          <div className="w-fit   md:justify-center">
+            <h1 for="text-input" class=" text-sm font-medium text-gray-600">
+              FullName
+            </h1>
+            <input
+              type="text"
+              id="text-input"
+              name="text-input"
+              class=" md:  mt-1  rounded-md border border-gray-300 p-2 md:mb-[50px]  md:w-[930px]"
+              placeholder="Enter text..."
+            />
+          </div>
+          <div className="  gap-5   md:flex md:w-[932px]  md:items-center md:justify-center md:justify-between md:justify-items-center  ">
+            <div className="md:justify-center">
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                UserName
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px] md:w-[446px]"
+                placeholder="Enter text..."
+              />
+
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                password
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                placeholder="Enter text..."
+              />
+
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                Date of Birth
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                placeholder="Enter text..."
+              />
+            </div>
+            <div>
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                Email
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px] md:w-[446px]"
+                placeholder="Enter text..."
+              />
+
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                ID Number
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                placeholder="Enter text..."
+              />
+
+              <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                Country
+              </h1>
+              <input
+                type="text"
+                id="text-input"
+                name="text-input"
+                class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                placeholder="Enter text..."
+              />
+            </div>
+          </div>
+          <br></br>
+          <div class="border-b-2 border-gray-300  shadow-md md:w-[930px] md:border-b-2 md:border-gray-300  md:shadow-md"></div>
+          Profile Picture
+          <div class="border-b-2 border-gray-300 shadow-md  md:w-[930px] md:border-b-2 md:border-gray-300 md:shadow-md  "></div>
+          <br></br>
+          <div>
+            <div className="md:mb-[50px] md:flex-col">
+              <h4 className=" text-sm">Credit Card</h4>
+            </div>
+            <div className="  gap-5   md:flex md:w-[932px]  md:items-center md:justify-center md:justify-between md:justify-items-center ">
+              <div className="md:justify-center">
+                <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                  Card Number
+                </h1>
+                <input
+                  type="text"
+                  id="text-input"
+                  name="text-input"
+                  class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px] md:w-[446px]"
+                  placeholder="Enter text..."
+                />
+
+                <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                  Expiry Date
+                </h1>
+                <input
+                  type="text"
+                  id="text-input"
+                  name="text-input"
+                  class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                  placeholder="Enter text..."
+                />
+              </div>
+              <div>
+                <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                  Card Owner
+                </h1>
+                <input
+                  type="text"
+                  id="text-input"
+                  name="text-input"
+                  class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px] md:w-[446px]"
+                  placeholder="Enter text..."
+                />
+
+                <h1 for="text-input" class="text-sm font-medium text-gray-600">
+                  CVC/CVV
+                </h1>
+                <input
+                  type="text"
+                  id="text-input"
+                  name="text-input"
+                  class="mt-1 w-full rounded-md border border-gray-300 p-2 md:mb-[50px]"
+                  placeholder="Enter text..."
+                />
+              </div>
+            </div>
+
+            <br></br>
+            <div className=" flex-col md:flex-col ">
+              <PrimaryBtn btnName="Register" />
+            </div>
+            <br></br>
+            <span>Already have an account?</span>
+            <a
+              href="https://www.example.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {"Login"}
+            </a>
+          </div>
+        </div>
+      </from>
+    </div>
+  );
 };
 
 export default Register;
