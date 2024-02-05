91952a24ca8ea5844f653bf82a710f8c74e803c4
Palm <palml1lax@gmail.com>
2024-02-05 12:28:51 +0700

    fix: fix admin database

--- src/app/api/admin/CustomerBooking/route.js
+++ src/app/api/admin/CustomerBooking/route.js
@@ -1,9 +1,11 @@
 import { NextResponse } from "next/server";
-import supabase from "../../../../utils/supabase.js";
+import { supabaseneung } from "@/lib/neungSUPABASE.js";
 
 export async function GET() {
   try {
-    const { data, error } = await supabase.from("CustomerBooking").select("*");
+    const { data, error } = await supabaseneung
+      .from("CustomerBooking")
+      .select("*");
     if (error) {
       console.error("Error fetching customer bookings:", error);
       return NextResponse.error("Error fetching customer bookings");
