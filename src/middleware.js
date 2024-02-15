5a19e180cec224585af486b636c302664953dce6
Palm <palml1lax@gmail.com>
2024-02-15 22:49:47 +0700

    fix:fix bug on middleware

--- src/middleware.js
+++ src/middleware.js
@@ -8,6 +8,7 @@ export async function middleware(request) {
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
   });
+  console.log(token);
   const { pathname } = request.nextUrl;
   if (token && token.role === "user" && pathname.startsWith("/user")) {
     return res;
