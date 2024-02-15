53ee9dffc9343b0c60e41d9f856af0efc0c27949
Palm <palml1lax@gmail.com>
2024-02-15 22:15:23 +0700

    fix: fix bug on edit_profile api

--- src/app/api/user/edit_profile/route.js
+++ src/app/api/user/edit_profile/route.js
@@ -1,30 +1,30 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+// import { prisma } from "@/lib/prisma";
+// import { NextResponse } from "next/server";
 
-export async function GET(request, { params: { user_id } }) {
-  console.log(user_id, "Hey");
-  try {
-    console.log(2);
-    const userProfileData = await prisma.userProfile.findUnique({
-      where: { user_id: +user_id },
-    });
-    console.log(3);
-    if (!userProfileData) {
-      return NextResponse.json({ error: "User Account not found" });
-    }
+// export async function GET(request, { params: { user_id } }) {
+//   console.log(user_id, "Hey");
+//   try {
+//     console.log(2);
+//     const userProfileData = await prisma.userProfile.findUnique({
+//       where: { user_id: +user_id },
+//     });
+//     console.log(3);
+//     if (!userProfileData) {
+//       return NextResponse.json({ error: "User Account not found" });
+//     }
 
-    console.log({ data: userProfileData });
-    return NextResponse.json({
-      success: true,
-      data: userProfileData,
-    });
-  } catch (error) {
-    console.log("Error fetching user profile...");
-    return NextResponse.json({
-      error: "Error fetching user profile",
-    });
-  }
-}
+//     console.log({ data: userProfileData });
+//     return NextResponse.json({
+//       success: true,
+//       data: userProfileData,
+//     });
+//   } catch (error) {
+//     console.log("Error fetching user profile...");
+//     return NextResponse.json({
+//       error: "Error fetching user profile",
+//     });
+//   }
+// }
 
 // export async function PUT(req, res) {
 //   const { userId } = req.query;
