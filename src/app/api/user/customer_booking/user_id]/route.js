66ee2bf84f31633125a015e66ef03f365965a775
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2024-02-19 17:47:40 +0700

    fixed: api booking_room

--- src/app/api/user/customer_booking/user_id]/route.js
+++ src/app/api/user/customer_booking/user_id]/route.js
@@ -1,21 +1,30 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params: { booking_id } }) {
+export async function GET(request, { params: { user_id } }) {
+  // console.log(booking_id);
   try {
-    const customerBooking = await prisma.customerBooking.findMany({
+    const userBookingProfile = await prisma.userProfile.findMany({
       where: {
-        id: +booking_id,
+        user_id: user_id,
       },
     });
-    console.log(customerBooking);
+    console.log(userBookingProfile);
+
+    if (!userBookingProfile) {
+      return NextResponse.json({ error: "User Account not found" });
+    }
+
     return NextResponse.json({
-      message: "Fetching booking_id data complete!",
-      data: customerBooking,
+      message: "Fetching user profile complete!",
+      data: userBookingProfile,
       status: 200,
     });
   } catch (error) {
-    return NextResponse.json({ message: "error" }, { status: 400 });
+    return NextResponse.json(
+      { message: "Error fetching user profile!" },
+      { status: 400 },
+    );
   }
 }
 
