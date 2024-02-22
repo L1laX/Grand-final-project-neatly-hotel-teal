2a6a9701073da01ffc12fde0cd8c130b99ffe342
Pao <chaiwatt77@gmail.com>
2024-02-22 13:44:01 +0700

    fix:booking page

--- src/app/api/user/customer_booking/[booking_id]/route.js
+++ src/app/api/user/customer_booking/[booking_id]/route.js
@@ -3,18 +3,51 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, { params: { booking_id } }) {
 
+    // const customerBooking = await prisma.user.findUnique({
+    //   where: {
+    //     id: booking_id,
+    //   },
+    //   include:{userProfile:true}
+    // });
+    //const {name} = await request.json()
+
+
+    const searchParams = new URLSearchParams(new URL(request.url).search);
+    const roomName = searchParams.get("roomName");
+    //console.log(roomName)
+
     const customerBooking = await prisma.user.findUnique({
       where: {
         id: booking_id,
       },
-      include:{userProfile:true}
+      select: {
+        name:true,
+        email: true,
+        userProfile: {
+          select: {
+            country: true,
+            id_number: true,
+            dateOfBirth: true,
+            payment_id:true
+          }
+        }
+      }
     });
+
+    const promotionCode = await prisma.promotion.findMany({
+      where: {
+        name: roomName
+      }
+    })
+    console.log(promotionCode)
+    
+
     console.log(customerBooking);
     return NextResponse.json({
       message: "Fetching booking_id data complete!",
       data: customerBooking,
-      status: 200,
-    });
+      promotionCode:promotionCode,
+    },{status:200});
 
 }
 
