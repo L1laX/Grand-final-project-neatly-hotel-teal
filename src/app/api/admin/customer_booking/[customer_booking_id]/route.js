d103a9d31bd584e9dc7e9cb3f9c777d82a48e111
shnItt1 <144312080+shnItt1@users.noreply.github.com>
2024-02-22 13:56:23 +0700

    add: booking history page

--- src/app/api/admin/customer_booking/[customer_booking_id]/route.js
+++ src/app/api/admin/customer_booking/[customer_booking_id]/route.js
@@ -1,23 +1,22 @@
 import { prisma } from "@/lib/prisma.js";
 import { NextResponse } from "next/server";
 
-export async function GET(request, { params: { customer_booking_id } }) {
-  console.log(customer_booking_id);
-  const customerId = customer_booking_id;
+export async function GET(request, { params: { user_id } }) {
+  console.log(user_id);
 
   try {
     const customerBookings = await prisma.customerBooking.findUnique({
-      where: {
-        id: customerId,
-      },
-      include: {
-        room: true,
-      },
+      where: { user_id: user_id },
     });
+
+    if (!customerBookings) {
+      return NextResponse.error("Customer booking not found", { status: 404 });
+    }
 
     return NextResponse.json({
       success: true,
       data: customerBookings,
+      status: 200,
     });
   } catch (error) {
     console.error("Error fetching customer bookings:", error);
--- src/app/user/[user_id]/booking_history/page.jsx
+++ src/app/user/[user_id]/booking_history/page.jsx
@@ -1,12 +1,46 @@
+"use client";
 import BookingCard from "@/components/common/BookingCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const BookingHistory = ({ params: customerId }) => {
+const BookingHistory = ({ user_id }) => {
+  // ต้องใช้ searchParams [user_id] และ [bookind_id]
+  // const user_id = searchParams.user_id;
+  // const booking_id = searchParams.booking_id;
+  // const path = `/user/${user_id}/booking_history/${booking_id}`;
+  // const url =
+  //   String(path) + "?user_id=" + user_id + "&booking_id=" + booking_id;
+
+  // const user_id = searchParams.user_id;
+
+  // const path = `/user/${user_id}/booking_history/`;
+  // const url = String(path) + "?user_id=" + user_id;
+
+  const [customerBooking, setCustomerBooking] = useState("");
+
+  const getBookingHistory = async () => {
+    try {
+      const res = await axios.get(`/api/admin/customer_booking/${user_id}/`);
+      console.log(res.data.data);
+      setCustomerBooking(res.data.data);
+    } catch (error) {
+      console.error("Error fetching customer bookings:", error);
+    }
+  };
+
+  console.log(customerBooking);
+
+  useEffect(() => {
+    getBookingHistory();
+  }, []);
+
   return (
     <div className="flex justify-center">
       <div className="flex w-11/12 max-w-[1980px] flex-col lg:w-5/6">
         <h2 className="py-20 text-4xl sm:text-5xl lg:text-7xl">
           Booking History
         </h2>
+        <h3>{customerBooking?.customerName}</h3>
         {/* Conditional Rendering List */}
         <BookingCard />
       </div>
