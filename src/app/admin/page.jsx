d97c0fa65dc71975096b1792abc8a7a0ca189fd1
Neung0511 <neung@gmail.com>
2024-02-23 17:48:13 +0700

    fixed(admin):data fetching for booking detail

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,255 +1,199 @@
 "use client";
-
-import React, { useEffect, useState } from "react";
-import Sidebar from "@/components/navbar/SidebarAdmin";
-import NavBar from "@/components/navbar/NavbarAdmin";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
+import Sidebar from "@/components/navbar/SidebarAdmin.jsx";
+import NavBarAdmin from "@/components/navbar/NavbarAdmin.jsx";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-function Admin({ params: { booking_id } }) {
-  const [booking, setBooking] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+function Admin() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const [rows, setRows] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        toast.info("Fetching Booking Details...", {
-          position: "top-center",
-          autoClose: false,
-        });
-
-        const response = await axios.get(
-          `/api/admin/customer_booking/${booking_id}`,
-        );
-        const data = response.data;
-
-        setBooking(data.data[0]); // Since data is an array, accessing its first element
-        setLoading(false);
-        toast.success("Booking Details Fetched Successfully!", {
-          position: "top-center",
-        });
-      } catch (error) {
-        console.error("Error fetching data from API:", error.message);
-        setError("Failed to fetch data. Please try again.");
-        toast.error("Failed to fetch Booking Details. Please try again.", {
-          position: "top-center",
-        });
-      } finally {
-        toast.dismiss();
-      }
+  const formatDate = (dateString) => {
+    const options = {
+      weekday: "short",
+      day: "numeric",
+      month: "short",
+      year: "numeric",
     };
-
-    fetchData();
-  }, [booking_id]);
-
-  if (loading) {
-    toast.info("Loading...", {
-      position: "top-center",
-      autoClose: false,
-    });
-  }
-
-  if (error) {
-    toast.error(`Error: ${error}`, {
-      position: "top-center",
-      autoClose: false,
-    });
-
-    return null;
-  }
-  if (!booking) {
-    toast.error("Failed to fetch booking details. Please try again.", {
-      position: "top-center",
-    });
-    return null;
-  }
-  const { room } = booking.customerBooking_room[0];
-
-  const { name, size, bedType, pricePerNight, roomAmenity, roomGallery } = room;
-
-  const {
-    customerName,
-    discount,
-    guestCount,
-    checkInDate,
-    checkOutDate,
-    paymentType,
-    totalPrice,
-    additionalRequest,
-    created_at,
-
-    user: { email: customerEmail },
-  } = booking;
-
-  const stayDuration = Math.floor(
-    (new Date(checkOutDate) - new Date(checkInDate)) / (1000 * 60 * 60 * 24),
-  );
-
-  const checkInDateObj = new Date(checkInDate);
-  const checkOutDateObj = new Date(checkOutDate);
-  const bookingDateObj = new Date(created_at);
-
-  const optionsFullDate = {
-    weekday: "short",
-    day: "numeric",
-    month: "short",
-    year: "numeric",
+    return new Date(dateString).toLocaleDateString("en-GB", options);
   };
 
-  const formattedCheckInDate = checkInDateObj.toLocaleDateString(
-    "en-US",
-    optionsFullDate,
-  );
-  const formattedCheckOutDate = checkOutDateObj.toLocaleDateString(
-    "en-US",
-    optionsFullDate,
-  );
+  const fetchData = async () => {
+    try {
+      toast.info("Fetching data...", {
+        position: "top-center",
+        autoClose: false,
+      });
 
-  const formattedBookingDate = bookingDateObj.toLocaleDateString(
-    "en-US",
-    optionsFullDate,
-  );
+      const response = await axios.get(
+        `/api/admin/customer_booking?keywords=${search}`,
+      );
+
+      const data = response.data;
+      setRows(data.data);
+      toast.dismiss();
+    } catch (error) {
+      console.error("Error fetching data from API:", error.message);
+      toast.error("Failed to fetch data. Please try again later.", {
+        position: "bottom-center",
+      });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [search]);
+
+  useEffect(() => {
+    console.log("Fetched Data:", rows);
+  }, [rows]);
+
+  const handleChangePage = (_event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleRowClick = (id) => {
+    router.push(`/admin/booking/${id}`);
+  };
+
+  const columns = [
+    {
+      id: "customerName",
+      label: "Customer name",
+      minWidth: 100,
+      align: "center",
+    },
+    { id: "guestCount", label: "Guest(s)", minWidth: 100, align: "center" },
+    { id: "roomType", label: "Room Type", minWidth: 100, align: "center" },
+
+    {
+      id: "totalPrice",
+      label: "Amount",
+      minWidth: 100,
+      align: "center",
+    },
+    { id: "bedType", label: "Bed Type", minWidth: 100, align: "center" },
+    { id: "checkInDate", label: "Check-in", minWidth: 100, align: "center" },
+    { id: "checkOutDate", label: "Check-out", minWidth: 100, align: "center" },
+  ];
+
+  const calculateStayDuration = (checkInDate, checkOutDate) => {
+    const checkIn = new Date(checkInDate);
+    const checkOut = new Date(checkOutDate);
+    const millisecondsPerDay = 1000 * 60 * 60 * 24;
+    const duration = (checkOut - checkIn) / millisecondsPerDay;
+    return Math.max(duration, 1);
+  };
 
   return (
-    <>
-      <div className="flex flex-row bg-gray-100">
-        <Sidebar />
-        <div className="flex w-full flex-col pl-4">
-          <NavBar
-            customerName={customerName}
-            navName={room.name}
-            buttonName={"Update"}
-            notSearch={true}
-            backarrow={true}
-            linkTo={`/admin/booking`}
-          />
-          <div
-            aria-label="Booking Details"
-            className="flex flex-col justify-center bg-slate-50 px-16 py-11 leading-[150%] text-slate-400 max-md:px-5"
+    <div className="flex flex-row bg-gray-100">
+      <Sidebar setActive={1} />
+      <div className="flex w-full flex-col">
+        <NavBarAdmin navName={"Customer Booking"} setSearch={setSearch} />
+        <div className="room-type-table mr-7 mt-16 flex items-center justify-center">
+          <Paper
+            sx={{ width: "100%", height: "100%", overflow: "hidden" }}
+            className="ml-10"
           >
-            <div className="flex flex-col rounded border border-solid border-[color:var(--gray-300,#E4E6ED)] bg-white px-20 py-11 max-md:max-w-full max-md:px-5">
-              <header className="text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Customer name{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {customerName}
-              </div>
+            <TableContainer sx={{ maxHeight: "100vh" }}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                        className="bg-gray-200 font-bold"
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.length === 0 ? (
+                    <TableRow>
+                      <TableCell colSpan={columns.length} align="center">
+                        No data available
+                      </TableCell>
+                    </TableRow>
+                  ) : (
+                    rows
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage,
+                      )
+                      .map((row) => {
+                        const roomDetails = row.customerBooking_room[0]?.room;
 
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Guest(s){" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {guestCount}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Room type{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {room.name}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Amount{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {room.pricePerNight * stayDuration}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Bed type{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {room.bedType}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Check-in{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {formattedCheckInDate}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Check-out{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {formattedCheckOutDate}
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Stay (total){" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {stayDuration} days
-              </div>
-
-              <header className="mt-10 text-xl font-semibold stacked-fractions tracking-tight max-md:max-w-full">
-                {" "}
-                Booking date{" "}
-              </header>
-              <div className="mt-1 text-base tracking-tight text-black max-md:max-w-full">
-                {formattedBookingDate}
-              </div>
-
-              <form
-                aria-label="Payment details"
-                className="mt-10 flex flex-col rounded bg-slate-50 px-6 py-4 text-slate-800 max-md:max-w-full max-md:px-5"
-              >
-                <div className="flex justify-between gap-4 px-20 pb-4 text-base tracking-tight text-slate-400 max-md:max-w-full max-md:flex-wrap max-md:px-5">
-                  <span>Payment success via</span>
-                  <span className="font-semibold">{paymentType}</span>
-                </div>
-
-                <div className="flex justify-between gap-4 whitespace-nowrap py-3 text-base tracking-tight max-md:max-w-full max-md:flex-wrap">
-                  <span>{room.name}</span>
-                  <span className="grow text-right font-semibold max-md:max-w-full">
-                    {room.pricePerNight}
-                  </span>
-                </div>
-
-                <div className="mt-2 flex justify-between gap-5 border-t border-solid border-t-[color:var(--gray-300,#E4E6ED)] pt-6 max-md:max-w-full max-md:flex-wrap">
-                  <div className="mt-1.5 flex-auto self-start text-base tracking-tight">
-                    {" "}
-                    Total{" "}
-                  </div>
-                  <span className="flex-auto text-right text-xl font-semibold stacked-fractions tracking-tight">
-                    {(
-                      room.pricePerNight * stayDuration -
-                      (discount ?? 0)
-                    ).toLocaleString("en-US", {
-                      style: "currency",
-                      currency: "THB",
-                    })}
-                  </span>
-                </div>
-              </form>
-
-              <div className="mb-2.5 mt-10 flex flex-col rounded bg-gray-200 px-6 py-4 text-base tracking-tight text-slate-500 max-md:max-w-full max-md:px-5">
-                <div className="font-semibold max-md:max-w-full">
-                  {" "}
-                  Additional Request{" "}
-                </div>
-                <div className="mt-2 max-md:max-w-full">
-                  {additionalRequest || "N/A"}
-                </div>
-              </div>
-            </div>
-          </div>
+                        return (
+                          <TableRow
+                            key={row.id}
+                            hover
+                            role="checkbox"
+                            tabIndex={-1}
+                            onClick={() => handleRowClick(row.id)}
+                            style={{ cursor: "pointer" }}
+                          >
+                            {columns.map((column) => {
+                              let value = row[column.id];
+                              if (column.id === "roomType") {
+                                value = roomDetails?.name || "N/A";
+                              } else if (column.id === "bedType") {
+                                value = roomDetails?.bedType || "N/A";
+                              } else if (
+                                column.id === "checkInDate" ||
+                                column.id === "checkOutDate"
+                              ) {
+                                value = formatDate(row[column.id]);
+                              }
+                              return (
+                                <TableCell key={column.id} align={column.align}>
+                                  {value}
+                                </TableCell>
+                              );
+                            })}
+                          </TableRow>
+                        );
+                      })
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </Paper>
         </div>
       </div>
       <ToastContainer />
-    </>
+    </div>
   );
 }
 
