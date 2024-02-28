5613e83a3124538d3d5b87d2846e5b6df9e6ed4e
Palm <palml1lax@gmail.com>
2024-02-28 22:30:04 +0700

    feat: add timeout

--- src/app/testPage/page.jsx
+++ src/app/testPage/page.jsx
@@ -1,46 +1,11 @@
-"use client";
-
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import axios from "axios";
 import React from "react";
-import CheckoutForm from "@/components/stripe/CheckoutForm";
-
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-);
-
-export default function TestPage() {
-  const [clientSecret, setClientSecret] = React.useState("");
-  const getClientSecret = async () => {
-    const response = await axios.post("/api/user/payment_method", {
-      amount: 1000,
-    });
-    console.log(response);
-    setClientSecret(response.data.clientSecret);
-  };
-  const appearance = {
-    theme: "stripe",
-    variables: {
-      colorPrimary: "#f79b45",
-      colorText: "#7a7b85",
-    },
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
-  React.useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    getClientSecret();
-  }, []);
+import Timeout from "@/components/common/Timeout";
+const page = () => {
   return (
-    <div className="main">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
+    <div>
+      <Timeout />
     </div>
   );
-}
+};
+
+export default page;
